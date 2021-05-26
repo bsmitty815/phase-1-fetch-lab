03d89a9a2595e140605fa3f6bdbb5ff7a7144d1e
Brian Smith <brian.bubbam@gmail.com>
2021-05-26 12:14:46 -0500

    commit tree 2035b7679073f44d10943728836faecda3300377

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
+  return fetch('https://anapioficeandfire.com/api/books')
+    .then(response => response.json())
+    .then(json => renderBooks(json));
+    //.then(json => console.log(json))
+
+ 
   
 }
 
